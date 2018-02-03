69d3bc34f54a8bec0365d573e472537779b21d45
Stephen Pon <sponis1@gmail.com>
2018-02-03 15:26:10 -0800

    basic scaffolding for pages

--- hackathon-cbt/src/app/app.module.ts
+++ hackathon-cbt/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ExerciseComponent } from './exercise/exercise.component';
+import { LogComponent } from './log/log.component';
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch:'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'exercise', component: ExerciseComponent },
+  { path: 'log', component: LogComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ExerciseComponent,
+    LogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ hackathon-cbt/src/app/exercise/exercise.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'excercise',
+  templateUrl: './exercise.component.html',
+  styleUrls: ['./exercise.component.css']
+})
+export class ExerciseComponent {
+  title = 'exercise';
+}
