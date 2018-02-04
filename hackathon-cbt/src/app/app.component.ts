be164c42ae36895348ff8fcaadd215aec2392ff7
Stephen Pon <sponis1@gmail.com>
2018-02-03 17:39:36 -0800

    adding ngoninit to appcomponent

--- hackathon-cbt/src/app/app.component.ts
+++ hackathon-cbt/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 declare var jQuery: any;
 
@@ -7,6 +7,10 @@ declare var jQuery: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  ngOnInit() {
+
+  }
 }
