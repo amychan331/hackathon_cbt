501da51268a95d4b966f884b9e1b7ffade0a9a21
amychan331@yahoo.com <amychan331@yahoo.com>
2018-02-03 18:43:32 -0800

    nav jquery function

--- hackathon-cbt/src/app/app.component.ts
+++ hackathon-cbt/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-import * as jquery from 'jquery';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +10,20 @@ export class AppComponent implements OnInit{
   title = 'app';
 
   ngOnInit() {
-  	jQuery('.meow').on('click', function() {
-  		console.log("KSDJFLKDSJ");
-  	})
+    // For mobile view navigation dropdown
+    $( ".fa-bars" ).click(function(e) {
+      e.preventDefault();
+      $(this).parent().next().toggle();
+    });
+    // CSS don't detect browser size change, so JS is use here
+    $(window).resize(function() {
+      if ($(window).width() > 812) {
+        $("nav ul:first").css({"display": "inline-block"});
+        $("nav li").css({"width": "auto"});
+      } else {
+        $("nav ul:first").css({"display": "none"});
+        $("nav li").css({"width": "100%"});
+      }
+    });
   }
 }
