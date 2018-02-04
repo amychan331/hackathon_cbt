c7891b5a72f8e92e3265529b3ca3441a6c86550b
Stephen Pon <sponis1@gmail.com>
2018-02-03 17:15:39 -0800

    install jquery

--- hackathon-cbt/src/typings.d.ts
+++ hackathon-cbt/src/typings.d.ts
@@ -3,3 +3,7 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+interface JQuery {
+   addClass(className: string): JQuery;
+   attr(attributeName: string, value: string|number): JQuery;
+}
